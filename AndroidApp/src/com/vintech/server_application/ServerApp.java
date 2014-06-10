d1973327404aa21d629da4404330d1b46c602bda
Vincent Gong <gongv93@gmail.com>
2014-06-10 10:50:41 -0400

    Initial Android commit

---
+++ AndroidApp/src/com/vintech/server_application/ServerApp.java
@@ -0,0 +1,22 @@
+package com.vintech.server_application;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ServerApp extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_server_app);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.server_app, menu);
+		return true;
+	}
+
+}
